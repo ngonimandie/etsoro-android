c395592f36f49a16f10a09de9fa9ccf6d257dd99
ngonimandie <ngonimandie@gmail.com>
2021-05-31 09:36:24 +0200

    Local commit on for ngrok test

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
 const BootBot = require('./lib/BootBot');
+const config= require ('config')
+
+const bot = new BootBot({
+    accessToken: config.get('access_token'),
+    verifyToken: config.get('verify_token'),
+    appSecret: config.get('app_secret')
+  });
+  
+  bot.on('message', (payload, chat) => {
+    const text = payload.message.text;
+    chat.say(`Echo: ${text}`);
+  });
+  
+  bot.start();
+  
 
 module.exports = BootBot;
