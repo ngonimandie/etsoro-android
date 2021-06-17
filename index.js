2a2daaeef84b68f0213b69c39ce0f361995f264e
ngonimandie <ngonimandie@gmail.com>
2021-06-17 08:48:28 +0200

    ports

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const echoModule = require('./examples/modules/echo');
 //Download file using express
 var express = require('express')
 var app = express()
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
+console.log('WEB APP  running on port', process.env.PORT);
 
 
 /*
